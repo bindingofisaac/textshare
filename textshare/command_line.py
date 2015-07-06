4f573db91535bf520ee4e635fc27c4090b41a9a0
bindingofisaac <bindingofisaacs@gmail.com>
2015-07-06 06:58:31 +0530

    supports multiple files

--- textshare/command_line.py
+++ textshare/command_line.py
@@ -1,6 +1,14 @@
 import click, textshare, os
 
 @click.command()
-@click.option('-f', '--file', type=click.Path(exists=True), help='filename to be shared', nargs=1, required=True)
-def cli(file):
-    click.echo(textshare.uploadfile(file))
+@click.option('--input', '-i', help='uses stdin as input', is_flag=True, default=False)
+@click.argument('filepaths', type=click.Path(exists=True), nargs=-1)
+def cli(input, filepaths):
+    if input:
+        pass
+    else:
+        if len(filepaths) != 0:
+            for fpath in filepaths:
+                click.echo(textshare.uploadfile(click.format_filename(fpath)))
+        else:
+            click.echo('pass atleast one filepath as argument or use -i/--input as an option')
