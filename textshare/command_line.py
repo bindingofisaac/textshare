65480aa6579fced64c0d8680556db513159349e2
bindingofisaac <bindingofisaacs@gmail.com>
2015-07-06 07:06:51 +0530

    read input from stdin

--- textshare/command_line.py
+++ textshare/command_line.py
@@ -1,11 +1,12 @@
-import click, textshare, os
+import click, textshare, sys
 
 @click.command()
 @click.option('--input', '-i', help='uses stdin as input', is_flag=True, default=False)
 @click.argument('filepaths', type=click.Path(exists=True), nargs=-1)
 def cli(input, filepaths):
     if input:
-        pass
+        text = sys.stdin.readlines()
+        click.echo(textshare.uploadtext(''.join(text)))
     else:
         if len(filepaths) != 0:
             for fpath in filepaths:
