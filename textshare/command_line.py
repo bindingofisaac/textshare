154ab418fd796c42c2d9ef0645512f7cb1f0c2e9
bindingofisaac <bindingofisaacs@gmail.com>
2015-07-07 00:17:29 +0530

    added map option

--- textshare/command_line.py
+++ textshare/command_line.py
@@ -2,14 +2,18 @@ import click, textshare, sys
 
 @click.command()
 @click.option('--input', '-i', help='uses stdin as input', is_flag=True, default=False)
+@click.option('--map', help='returns output as a map of filepaths and url', is_flag=True, default=False)
 @click.argument('filepaths', type=click.Path(exists=True), nargs=-1)
-def cli(input, filepaths):
+def cli(input, map, filepaths):
     if input:
         text = sys.stdin.readlines()
         click.echo(textshare.uploadtext(''.join(text)))
     else:
         if len(filepaths) != 0:
             for fpath in filepaths:
-                click.echo(textshare.uploadfile(click.format_filename(fpath)))
+                map_fpath = fpath;
+                if not map:
+                    map_fpath = ""
+                click.echo(map_fpath+" : "+textshare.uploadfile(click.format_filename(fpath)))
         else:
             click.echo('pass atleast one filepath as argument or use -i/--input as an option')
